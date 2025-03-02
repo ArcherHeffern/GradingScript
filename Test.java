6b08e22eccf616712701e9b310c0a2b2642e8871
ArcherHeffern <heffernarcher@gmail.com>
2025-03-02 16:56:39 -0500

    parses student submission into expected structure

--- Test.java
+++ Test.java
@@ -1,5 +1,24 @@
-public class Main {
-	public static void main(String[] args) {
-		PCB.PrintHello();
+
+package main;
+import java.io.File;
+import java.io.IOException;
+
+public class Test {
+	public static void main(String[] args) throws IOException {
+		if (args.length != 2) {
+			System.out.println("Usage [destfile]");
+			System.exit(1);
+		}
+
+		runTests();
+
+		if (!new File(args[1]).createNewFile()) {
+			throw new IOException();	
+		}
+	}
+
+	private static void runTests() {
+		System.out.println("Hello world");
+		// PCB.PrintHello();
 	}
 }
--- grade.sh
+++ grade.sh
@@ -5,6 +5,8 @@ set -euo pipefail
 # TODO
 # - Support comparing student output with expected output and saving as diff
 # - Cd doesn't check for errors
+# - Verify the test script is ran and student doesn't override it 
+
 
 command -v fd >/dev/null 2>&1 || { echo >&2 "Script requires 'fd' but it's not installed.  Aborting."; exit 1; }
 
@@ -25,10 +27,12 @@ fi
 
 # Globals: All paths should be relative to this executable
 ZIPPED="zipped/"
-UNZIPPED="unzipped/"
+UNCLEAN_UNZIPPED="unclean_unzipped/"
+CLEAN_UNZIPPED="clean_unzipped/"
 RESULTS="results/"
 TEST_CLASS="Test.java"
-PROJECT_CLASSES=("PCB.java" "ProcessManager.java" "Queue.java")
+TEST_CLASS_DEST="main/"
+PROJECT_CLASSES=("main/PCB.java" "main/ProcessManager.java" "main/Queue.java")
 EXPECTED_OUTPUT="expected.txt" # Not used
 MOODLE_SUBMISSION_EXTENSION="_assignsubmission_file"
 
@@ -39,8 +43,8 @@ then
 fi
 
 # Reset: Remove $ZIPPED, $UNZIPPED, and $RESULTS
-rm -rf "${ZIPPED}" "${UNZIPPED}" "${RESULTS}"
-mkdir -p "${ZIPPED}" "${UNZIPPED}" "${RESULTS}"
+rm -rf   "${ZIPPED}" "${UNCLEAN_UNZIPPED}" "${CLEAN_UNZIPPED}" "${RESULTS}"
+mkdir -p "${ZIPPED}" "${UNCLEAN_UNZIPPED}" "${CLEAN_UNZIPPED}" "${RESULTS}"
 
 # Post processing of variables - Don't touch!
 RESULTS="$(realpath "${RESULTS}")/"
@@ -50,29 +54,75 @@ TEST_CLASS_ABS_PATH="$(realpath "${TEST_CLASS}")"
 unzip "${INPUT_ZIPFILE}" -d "${ZIPPED}" > /dev/null
 
 # Process all submissions
-mapfile -t projects < <(fd --full-path -I -e=zip "${MOODLE_SUBMISSION_EXTENSION}" "${ZIPPED}")
+mapfile -t student_submissions_zipped < <(fd --full-path -I -e=zip "${MOODLE_SUBMISSION_EXTENSION}" "${ZIPPED}")
 
-for project in "${projects[@]}"; do
-	# Unzip submission and place in $ZIPPED
-	student="$(basename "${project::-4}")"
-	echo "=== Running ${student}'s submission ==="
-	dest="${UNZIPPED}${student}"
-	if ! unzip -q "${project}" -d "${dest}";
+for student_submission_zipped in "${student_submissions_zipped[@]}"; do
+	student_id="$(basename "${student_submission_zipped::-4}")"
+	echo "=== Running ${student_id}'s submission ==="
+
+	# ============
+	# Unzip submission and place in $UNCLEAN_UNZIPPED/$student_id
+	# ============
+	student_submission_unzipped_unclean="${UNCLEAN_UNZIPPED}${student_id}/"
+	if ! unzip -q "${student_submission_zipped}" -d "${student_submission_unzipped_unclean}";
 	then
-		echo "Failed to unzip ${project}"
+		echo "Failed to unzip ${student_submission_zipped}"
 		continue
 	fi
 
+	# ============
+	# Submission Cleaning and Setup
+	# ============
+	# - Verify script contains all $PROJECT_CLASSES
+	# - Move all project classes to $CLEAN_UNZIPPED/$student_id
+	# - copy $TEST_CLASS to $CLEAN_UNZIPPED/$student_id/$TEST_CLASS_DEST/$TEST_CLASS
+	ok=true
+	student_submission_unzipped_clean="${CLEAN_UNZIPPED}${student_id}/"
+	for PROJECT_CLASS in "${PROJECT_CLASSES[@]}"; do
+		mapfile -t project_class_unclean_location < <(fd -Ipt file "${PROJECT_CLASS}" "${student_submission_unzipped_unclean}")
+		num_file_matches="${#project_class_unclean_location[@]}"
+		if [[ "${num_file_matches}" -gt 1 ]];
+		then
+			echo "${student_id}: Too many files matching ${PROJECT_CLASS}. Aborting..."
+			ok=false
+			break
+		elif [[ "${num_file_matches}" -lt 1 ]];
+		then
+			echo "${student_id}: No files matching ${PROJECT_CLASS}. Aborting..."
+			ok=false
+			break
+		fi 
+		clean_dest="${student_submission_unzipped_clean}${PROJECT_CLASS}"
+		mkdir -p "$(dirname "${clean_dest}")"
+		mv "${project_class_unclean_location[0]}" "${clean_dest}"
+	done
+	if ! $ok; then
+		continue
+	fi
+	test_file_dest_dir="${CLEAN_UNZIPPED}${student_id}/${TEST_CLASS_DEST}"
+	mkdir -p "${test_file_dest_dir}"
+	cp "${TEST_CLASS_ABS_PATH}" "${test_file_dest_dir}${TEST_CLASS}"
+	continue
+
+	# ============
+	# Execute Program Securely
+	# ============
+	# TODO
+	# - Create new user
+	# - Switch permission of dest to user. 
+	# - Run program as user
 	# Run submission and write output to $OUTPUT
-	cd "${dest}"
-	cp "${TEST_CLASS_ABS_PATH}" .
-	result_dest="${RESULTS}${student}"
+	cd "${student_submission_unzipped_clean}"
+	
+	result_dest="${RESULTS}${student_id}"
 	if ! java "${TEST_CLASS}" "${PROJECT_CLASSES[@]}" &> "${result_dest}";
 	then
-		echo "Failed to run ${student}'s submission"
+		echo "Failed to run ${student_id}'s submission"
 	fi
+
+	# TODO: Switch back to root. Delete user. cd back
 	cd - > /dev/null
 done
 
 # Clean up
-rm -rf "${ZIPPED}" "${UNZIPPED}" 
+rm -rf "${ZIPPED}" "${UNCLEAN_UNZIPPED}" 
